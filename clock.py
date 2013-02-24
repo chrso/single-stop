581a05cfec86ea93232e534eb64f8cd8c03b26b0
Kevin Roark Jr <ker2143@columbia.edu>
2013-02-24 03:57:42 -0500

    fixed appt buttons

--- clock.py
+++ clock.py
@@ -1,1 +1,18 @@
 from apscheduler.scheduler import Scheduler
+
+sched = Scheduler()
+
+@sched.interval_schedule(minutes=3)
+def timed_job():
+  print 'this job is run every three minutes'
+
+@sched.cron_schedule(day_of_weeks='mon-fri', hour=17)
+def scheduled_job():
+  print 'this job is run every weekday at 5pm'
+
+
+
+sched.start()
+
+while True:
+  pass
