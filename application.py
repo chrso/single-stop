abbd3a552f75626141c9a4ee69438add9a065e60
Christopher So <christopherso92@gmail.com>
2013-02-23 21:53:11 -0500

    changed some permissions/redirects

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(25), unique=True)
+    password = db.Column(db.String(25))
     phone_number = db.Column(db.String(20))
 
     def __init__(self, username, email, password, phone_number):
@@ -158,6 +158,8 @@ def register_user():
     user = User(request.form['username'], request.form['email'], request.form['password'], "")
     db.session.add(user)
     db.session.commit()
+
+    session['logged_in'] = True 
 
     return redirect(url_for('student'))
 
